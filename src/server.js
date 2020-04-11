23fa600158b472cae90f0837a3c6b08c26912007
ocomaster <brayan-bamv@hotmail.com>
2020-04-11 02:46:30 -0500

    Rutas del servidor con express

--- src/server.js
+++ src/server.js
@@ -22,9 +22,7 @@ app.use(express.urlencoded({ extended: false}));
 //Global variables
 
 //routes
-app.get('/', (req, res) =>{
-    res.render('index')
-});
+app.use(require('./routes/index.routes'));
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
